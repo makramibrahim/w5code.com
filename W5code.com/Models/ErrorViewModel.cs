fd4054d89a619d1d4bda65f8ad8588da048f60ed
makramibrahim <makram3ibrahim@gmail.com>
2018-04-05 20:03:19 -0600

    Add project files.

---
+++ W5code.com/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace W5code.com.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
